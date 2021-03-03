e0c6abe311a8d43ee6a74c2c82ca4c5fa3deee83
aarongermaine <vcnts0@gmail.com>
2021-03-03 15:48:58 -0700

    question and package additions

--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,60 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
 
+const writeFileAsync = util.promisify(fs.writeFile);
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "What is the title of your project?",
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Please enter a description of your project",
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: "Please enter instructions on how to install your project",
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "Please enter steps needed to be taken to use the project",
+    },
+    {
+      type: "input",
+      name: "contribution",
+      message: "Who can contribute and how?",
+    },
+    {
+      type: "input",
+      name: "test",
+      message: "Please enter instructions for testing",
+    },
+    {
+      type: "checkbox",
+      message: "What license would you like to use?",
+      name: "license",
+      choices: ["MIT", "GNU", "Mozilla", "Unlicense"],
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "What is your Github username?",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "Please enter your business email",
+    },
+  ]);
+};
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
